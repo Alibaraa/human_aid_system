2b63e7be5b4f90b7c86a180d5c74629fe7cf8856
unknown <alialagha455@gmail.com>
2025-11-01 10:57:38 +0200

    after edit htaccess

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * CodeIgniter
+ *
+ * This file redirects to the public folder where the actual
+ * application entry point is located.
+ */
+
+// Redirect to public/index.php
+header('Location: ./public/index.php');
+exit();
