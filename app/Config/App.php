7d6e47e9c7c371c58d65ce38fc4b6d1cd8cdc946
unknown <alialagha455@gmail.com>
2025-11-01 11:09:00 +0200

    after edit App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://Ali/fajer/public/';
+    public $baseURL = '';
 
     /**
      * --------------------------------------------------------------------------
@@ -36,7 +36,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $indexPage = 'index.php';
+    public $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
