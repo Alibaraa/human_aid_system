875554b20edbb994fda9820105d28e2e8f116ab6
unknown <alialagha455@gmail.com>
2025-11-17 14:45:11 +0200

    after edit UserActionSaved.php

--- app/Filters/UserActionSaved.php
+++ app/Filters/UserActionSaved.php
@@ -19,7 +19,7 @@ class UserActionSaved implements FilterInterface
     public function before(RequestInterface $request, $arguments = null)
     {
         if(!isset($this->session->get('userData')['id'])){
-            return redirect()->route('login');
+            return redirect()->to(base_url('login_page/signin-1.html'));
         }
 
         $router = service('router');
